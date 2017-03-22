fdecb6ae687c78d12049e9d19a41f42dfa502a4b
Oliver Gassner <og@plan2.net>
2017-03-22 08:49:42 +0000

    add composer.json, 7.6 working

---
+++ ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "macina_searchbox".
+ *
+ * Auto generated 07-08-2013 11:10
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Searchbox for Indexed Search Engine',
+	'description' => 'This Extension offers the possibility to add a searchbox to your Typo3 powered website which submits the searchvalue to the Indexed Search Engine	plugin. The extension has a html template which is easy to modify and supports multiple languages.',
+	'category' => 'plugin',
+	'shy' => '0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => '1',
+	'lockType' => '',
+	'author' => 'Wolfgang Becker',
+	'author_email' => 'wb@macina.com',
+	'author_company' => '',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'version' => '6.2.0',
+	'constraints' =>
+	array (
+		'depends' =>
+		array (
+			'typo3' => '6.2.0 - 7.6.99',
+			'php' => '',
+			'cms' => '',
+			'indexed_search' => '',
+		),
+	),
+);
+
+?>
